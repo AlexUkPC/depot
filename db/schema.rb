3a84c26cdb48b131f3a74986381146bc9145fe24
AlexUkPC <alexrogna@yahoo.com>
2021-01-27 13:20:14 +0000

    migrate scaffolded Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_154210) do
+ActiveRecord::Schema.define(version: 2021_01_27_131826) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
