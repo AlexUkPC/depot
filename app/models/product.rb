a065089154e00dfedef31b99e3c0248779745259
AlexUkPC <alexrogna@yahoo.com>
2021-01-27 13:40:57 +0000

    typo

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,7 @@ validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
 private
   def ensure_not_referenced_by_any_line_item
-    unless line_item.empty?
+    unless line_items.empty?
       errors.add(:base, "Line Items present")
       throw :abort
     end
