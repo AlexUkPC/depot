71fdbb877dd7b43c370c854e5705258c784f69cb
AlexUkPC <alexrogna@yahoo.com>
2021-01-28 20:31:29 +0000

    typo

--- app/jobs/charge_order_job.rb
+++ app/jobs/charge_order_job.rb
@@ -2,6 +2,6 @@ class ChargeOrderJob < ApplicationJob
   queue_as :default
 
   def perform(order, pay_type_params)
-    order.charge!(pay_type_params)
+    order.change!(pay_type_params)
   end
 end
