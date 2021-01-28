c00172331f23128998ff4db5da69640e7694ce21
AlexUkPC <alexrogna@yahoo.com>
2021-01-28 20:31:29 +0000

    typo

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
-    "Purchase oreder" => 2
+    "Purchase order" => 2
   }
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
