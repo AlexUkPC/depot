ad1970b5d2f66f1657c3a248fe0f197e28728d34
AlexUkPC <alexrogna@yahoo.com>
2021-01-29 12:58:13 +0000

    define i18n languages and places where to be used

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,17 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "\letter_opener"
   end
