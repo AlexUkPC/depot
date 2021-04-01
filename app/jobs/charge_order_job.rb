6c1c6357470c5b272109fec95b904974451b071a
AlexUkPC <alexrogna@yahoo.com>
2021-04-01 09:16:14 +0100

    moved on wsl

--- app/jobs/charge_order_job.rb
+++
@@ -1,7 +0,0 @@
-class ChargeOrderJob < ApplicationJob
-  queue_as :default
-
-  def perform(order, pay_type_params)
-    order.change!(pay_type_params)
-  end
-end
--- app/mailboxes/application_mailbox.rb
+++
@@ -1,3 +0,0 @@
-class ApplicationMailbox < ActionMailbox::Base
-  routing "support@example.com" => :support
-end
